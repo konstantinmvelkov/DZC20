f3b42ed1839d22d43e917193bdc37dbffa90ecc0
tini <konstantinmvelkov@gmail.com>
2020-12-09 17:37:32 +0100

    Player movement by one unit - left,right rotation and forward.
    Implemented by couroutines which can easily be called from another function (when we decide how we want the player to input their actions)

--- Assets/Stylized Astronaut/Character/Player.cs
+++ Assets/Stylized Astronaut/Character/Player.cs
@@ -1,35 +1,79 @@
 using UnityEngine;
 using System.Collections;
+using UnityEditor;
+using System;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-		private Animator anim;
-		private CharacterController controller;
+    private Animator anim;
+    private CharacterController controller;
 
-		public float speed = 600.0f;
-		public float turnSpeed = 400.0f;
-		private Vector3 moveDirection = Vector3.zero;
-		public float gravity = 20.0f;
+    public float speed = 200.0f;
+    public float turnSpeed = 400.0f;
+    public float gravity = 20.0f;
+    public int interpolationFramesCount = 45; // Number of frames to completely interpolate between the 2 positions
 
-		void Start () {
-			controller = GetComponent <CharacterController>();
-			anim = gameObject.GetComponentInChildren<Animator>();
-		}
+    bool isTranslating = false;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = gameObject.GetComponentInChildren<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && !isTranslating)
+        {
+            anim.SetInteger("AnimationPar", 1);
+            isTranslating = true;
+            StartCoroutine(Rotate(Vector3.up, -90, 0.2f));
+        }
+        if (Input.GetKeyDown(KeyCode.D) && !isTranslating)
+        {   
+            anim.SetInteger("AnimationPar", 1);
+            isTranslating = true;
+            StartCoroutine(Rotate(Vector3.up, 90, 0.2f));
+        }
 
-		void Update (){
-			if (Input.GetKey ("w")) {
-				anim.SetInteger ("AnimationPar", 1);
-			}  else {
-				anim.SetInteger ("AnimationPar", 0);
-			}
+        if (Input.GetKeyDown(KeyCode.W) && !isTranslating)
+        {
+            anim.SetInteger("AnimationPar", 1);
+            isTranslating = true;
+            StartCoroutine(SmoothLerp(0.3f));
+        }
 
-			if(controller.isGrounded){
-				moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
-			}
+        IEnumerator SmoothLerp(float time)
+        {
+            Vector3 startingPos = transform.position;
+            Vector3 finalPos = transform.position + transform.forward;
+            float elapsedTime = 0;
 
-			float turn = Input.GetAxis("Horizontal");
-			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
-			controller.Move(moveDirection * Time.deltaTime);
-			moveDirection.y -= gravity * Time.deltaTime;
-		}
+            while (elapsedTime < time)
+            {
+                transform.position = Vector3.Lerp(startingPos, finalPos, (elapsedTime / time));
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            isTranslating = false;
+            anim.SetInteger("AnimationPar", 0);
+        }
+
+        IEnumerator Rotate(Vector3 axis, float angle, float duration = 1.0f)
+        {
+            Quaternion from = transform.rotation;
+            Quaternion to = transform.rotation;
+            to *= Quaternion.Euler(axis * angle);
+
+            float elapsed = 0.0f;
+            while (elapsed < duration)
+            {
+                transform.rotation = Quaternion.Slerp(from, to, elapsed / duration);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+            isTranslating = false;
+            anim.SetInteger("AnimationPar", 0);
+            transform.rotation = to;
+        }
+    }
 }
