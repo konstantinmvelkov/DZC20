e9ce0012a839dd403c0aed8aa283501e4382f108
NickJeurissen <64475952+NickJeurissen@users.noreply.github.com>
2020-12-13 19:27:15 +0100

    Added Ramp Prefab and vertical movement
    - Added Ramp Prefab (Assets/PolygonStarter/Models)
    
    - Added vertical movement in player.cs
    
    - Added 2 tags (used by player.cs)
    StairsUp
    StairsDown
    
    Ramp will only work if placed on proper height else script won't detect the up/down triggers.

--- Assets/SineVFX/TranslucentCrystals/Prefabs/CrystalHandler.cs
+++ Assets/SineVFX/TranslucentCrystals/Prefabs/CrystalHandler.cs
@@ -11,11 +11,14 @@ public class CrystalHandler : MonoBehaviour
     public int TotalCrystals;
 
     Text text;
+    
+    //private Slider slider;
 
     // Start is called before the first frame update
     void Start()
     {
         text=GameObject.Find("CrystalScore").GetComponent<Text>();
+        //slider=GameObject.Find("CrystalSlider").GetComponent<Slider>();
         CrystalCount = 0;
         CrystalList = GameObject.FindGameObjectsWithTag("Crystal"); //List of active crystal gameobjects in scene
         TotalCrystals = CrystalList.Length; //Amount of active crystals at start of scene
@@ -31,6 +34,8 @@ public class CrystalHandler : MonoBehaviour
         //Debug.Log("Total crystals in scene: "+ TotalCrystals);
         //Debug.Log("Collected Crystals: "+ CrystalCount);
         text.text="Collected Crystals: "+CrystalCount.ToString()+" / "+ TotalCrystals.ToString();
+
+        //slider.value = (CrystalCount/TotalCrystals);
         
     }
 }
--- Assets/Stylized Astronaut/Character/Player.cs
+++ Assets/Stylized Astronaut/Character/Player.cs
@@ -16,10 +16,20 @@ public class Player : MonoBehaviour
     public int interpolationFramesCount = 45; // Number of frames to completely interpolate between the 2 positions
 
     bool isTranslating = false;
+
+    int stepCount;
+
+    bool UpLastTick = true;
+    bool DownLastTick = false;
+
+
+
+    
     void Start()
     {
         controller = GetComponent<CharacterController>();
         anim = gameObject.GetComponentInChildren<Animator>();
+        stepCount = 0;  // Counts the amount of steps the player has taken so far. (is not used yet)
     }
     void Update()
     {
@@ -27,12 +37,14 @@ public class Player : MonoBehaviour
         {
             anim.SetInteger("AnimationPar", 1);
             isTranslating = true;
+            stepCount++;
             StartCoroutine(Rotate(Vector3.up, -90, 0.2f));
         }
         if (Input.GetKeyDown(KeyCode.D) && !isTranslating)
         {   
             anim.SetInteger("AnimationPar", 1);
             isTranslating = true;
+            stepCount++;
             StartCoroutine(Rotate(Vector3.up, 90, 0.2f));
         }
 
@@ -40,13 +52,35 @@ public class Player : MonoBehaviour
         {
             anim.SetInteger("AnimationPar", 1);
             isTranslating = true;
+            stepCount++;
             StartCoroutine(SmoothLerp(0.3f));
         }
-
         IEnumerator SmoothLerp(float time)
         {
             Vector3 startingPos = transform.position;
             Vector3 finalPos = transform.position + transform.forward;
+            
+            Collider[] Intersecting = Physics.OverlapSphere(startingPos, 0.02f); // This detects intersecting objects including the plate that indicates stairs(>0.02f detects ground)
+            // Might have to change the value of 0.02f if we use a different model for the robot...
+            
+            if (Intersecting.Length != 0) //This is true if there are intersecting objects nearby, triggers, crystals etc..
+            {
+                for (var i = 0; i < Intersecting.Length; i++) 
+                {
+                if (Intersecting[i].tag == "StairsUp" && transform.eulerAngles == Intersecting[i].transform.eulerAngles) //Stairs up condition
+                    {
+                        finalPos = transform.position + transform.forward + 0.25f*transform.up; //Modify final pos to include height difference
+                        //Debug.Log(Intersecting[i].name);
+                    }
+                if (Intersecting[i].tag == "StairsDown" && transform.eulerAngles == Intersecting[i].transform.eulerAngles) //Stairs down condition
+                    {
+                        finalPos = transform.position + transform.forward - 0.25f*transform.up; //Modify final pos to include height difference
+                        // Debug.Log(Intersecting[i].name);
+                    }
+                }
+                
+            }
+
             float elapsedTime = 0;
 
             while (elapsedTime < time)
@@ -76,5 +110,29 @@ public class Player : MonoBehaviour
             anim.SetInteger("AnimationPar", 0);
             transform.rotation = to;
         }
+
+
+    }
+    private void HeightCheck()
+    {
+        Debug.Log("Raycast script running");
+        Vector3 origin = transform.position + 0.5f*transform.up;
+        Vector3 direction = -transform.up;
+        Debug.Log(origin);
+        Debug.Log(direction);
+        Debug.DrawRay(origin, direction * 10f, Color.red);
+        Ray ray = new Ray(origin, direction);
+        if (Physics.Raycast(origin, direction, 0.6f) && !UpLastTick)
+        {
+            transform.position = transform.position + 0.01f * transform.up;
+            UpLastTick = true;
+            DownLastTick = false;
+            }
+        else{
+            transform.position = transform.position - 0.01f * transform.up;
+            UpLastTick = false;
+            DownLastTick = true;
+            }
+
     }
 }
